a202941526a332e64291fcce0a79d58bd84c7245
JSRamirezC <johan9106@gmail.com>
2022-07-31 16:56:43 -0500

    archivos de configuración

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CustomModule } from './custom-material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { SimpleNotificationsModule } from 'angular2-notifications';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientService } from './services/http-client.service';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +20,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule,
+    FormsModule,
+    CustomModule,
+    BrowserAnimationsModule,
+    SimpleNotificationsModule.forRoot(),
+    FlexLayoutModule
   ],
-  providers: [],
+    
+    providers: [HttpClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/http-client.service.ts
@@ -0,0 +1,113 @@
+import { Injectable } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class HttpClientService {
+
+  constructor(private httpClient: HttpClient, 
+    private notificationsService: NotificationsService
+    ) {
+  }
+
+  private agregarHeadersGenericos() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    //if (sessionStorage.getItem('token')) { headers = headers.append('token', sessionStorage.getItem('token')) }
+    return { headers: headers, observe: 'response' as 'body' };
+  }
+
+  private agregarHeadersArchivos() {
+    let headers = new HttpHeaders();    
+    if (sessionStorage.getItem('origenToken')) { headers = headers.append('origenToken', sessionStorage.getItem('origenToken') || "[]") }
+    if (sessionStorage.getItem('token')) { headers = headers.append('token', sessionStorage.getItem('token') || "[]") }
+    return { headers: headers, observe: 'response' as 'body', responseType: 'blob' as 'json' };
+  }
+
+  private agregarHeadersInternos() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    if (sessionStorage.getItem('token')) { headers = headers.append('token', sessionStorage.getItem('token') || "[]") }
+    return { headers: headers, observe: 'response' as 'body' };
+  }
+
+  get(url: string) {
+    return this.httpClient.get(url, this.agregarHeadersGenericos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  getInterno(url: string) {
+    return this.httpClient.get(url, this.agregarHeadersInternos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  getArchivo(url: string) {
+    return this.httpClient.get(url, this.agregarHeadersArchivos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  post(url: string, body?: any) {
+    return this.httpClient.post(url, body, this.agregarHeadersGenericos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  postInterno(url: string, body?: any) {
+    return this.httpClient.post(url, body, this.agregarHeadersInternos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  postArchivo(url: string, body?: any) {
+    return this.httpClient.post(url, body, this.agregarHeadersArchivos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  delete(url: string) {
+    return this.httpClient.delete(url, this.agregarHeadersGenericos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  put(url: string, body?: any) {
+    return this.httpClient.put(url, body, this.agregarHeadersGenericos())
+      .pipe(catchError(err => {
+        this.handleError(err);
+        return throwError(err);
+      }));
+  }
+
+  logOut() {
+    setTimeout(() => {
+      sessionStorage.clear();
+    }, 1000);
+  }
+
+  handleError(err: any) {
+    if (err.status == 401) { 
+      this.logOut();
+    } else if (err.status == 403) {
+       alert('Usted no tiene permiso para la acción solicitada');
+    }
+  }
+}
